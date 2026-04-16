c91ed79ad70105dd82ce2a2261c964eac70da65c
Chad <chdcosta@gmail.com>
2026-04-15 17:22:59 -0700

    fix: wire main.jsx to use collabmix-production.jsx for all pages - landing, lobby, session now unified

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-import { StrictMode, useState } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Landing from './App.jsx'
 import CollabMix from './collabmix-production.jsx'
-
-function Root() {
-  const [djName, setDjName] = useState(null)
-  if (djName !== null) return <CollabMix initialPage="lobby" djName={djName} />
-  return <Landing onLaunch={(name) => setDjName(name || "")} />
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Root />
+    <CollabMix initialPage="landing" />
   </StrictMode>,
 )
